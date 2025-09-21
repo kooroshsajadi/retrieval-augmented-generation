99f06f299df739a586cba525cdacd1a38a526c89
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2025-09-21 16:40:42 +0200

    Add model loader mapping.

--- src/utils/models/model_types.py
+++ src/utils/models/model_types.py
@@ -1,9 +1,19 @@
-from transformers import AutoModel, AutoModelForSeq2SeqLM
+from transformers import AutoModel, AutoModelForCausalLM, AutoModelForSeq2SeqLM
 from enum import Enum
+from src.models.causal_model_loader import CausalModelLoader
+from src.models.encoder_only_model_loader import EncoderOnlyModelLoader
+from src.models.seq2seq_model_loader import Seq2SeqModelLoader
 
 MODEL_TYPE_MAPPING = {
     "seq2seq": AutoModelForSeq2SeqLM,
-    "encoder-only": AutoModel
+    "encoder-only": AutoModel,
+    "causal": AutoModelForCausalLM
+}
+
+MODEL_LOADER_MAPPING = {
+    "seq2seq": Seq2SeqModelLoader,
+    "encoder-only": EncoderOnlyModelLoader,
+    "causal": CausalModelLoader
 }
 
 class ModelTypes(Enum):
