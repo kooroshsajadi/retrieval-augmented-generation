baab1700c464d8f8ec78533ed0fb774ec3033d15
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2025-09-26 12:19:31 +0200

    Add proper code to get the length of a collection.

--- src/data/milvusdb_observer.py
+++ src/data/milvusdb_observer.py
@@ -1,14 +1,18 @@
-from pymilvus import connections, utility
+from pymilvus import Collection, connections, utility
 
 # Connect to Milvus
 connections.connect(alias="default", host="localhost", port="19530")
 
-# List collections before deletion
-print("Collections before removal:", utility.list_collections())
+# List collections.
+print("Collections:", utility.list_collections())
 
 # Drop the collection
 # utility.drop_collection("test_gotmat_collection")
 # print("Dropped collection: test_gotmat_collection")
 
-# List collections after deletion to confirm removal
-print("Collections after removal:", utility.list_collections())
+# Get the collection
+collection = Collection("gotmat_collection")
+
+# Get the number of entities in the collection
+num_entities = collection.num_entities
+print(f"Number of entities in the collection: {num_entities}")
